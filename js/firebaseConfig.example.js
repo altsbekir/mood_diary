9fa18b6b764018e1713f24b7d60764a474a6f320
Bekir Alataş <bekiralatas2332@gmail.com>
2026-03-02 16:06:50 +0300

    feat: firebaseConfig dosyası güncellendi.

---
+++ js/firebaseConfig.example.js
@@ -0,0 +1,10 @@
+// js/firebaseConfig.example.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyCDY_Wf-mPJZAI8t3HfiD43aDXaMbwnOx4",
+    authDomain: "mood-diary-2cce3.firebaseapp.com",
+    projectId: "mood-diary-2cce3",
+    storageBucket: "mood-diary-2cce3.firebasestorage.app",
+    messagingSenderId: "460407557020",
+    appId: "1:460407557020:web:6f082060833ae7591a9659",
+    measurementId: "G-VLYWPC9F8S"
+};
