06af19f6ebd09d264ca5c6f0a82833b08c7a44f0
Bekir Alataş <bekiralatas2332@gmail.com>
2026-03-02 11:13:14 +0300

    sec: hassas veri .env dosyasına taşındı ve gitignore güncellendi

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDY_Wf-mPJZAI8t3HfiD43aDXaMbwnOx4",
-  authDomain: "mood-diary-2cce3.firebaseapp.com",
-  projectId: "mood-diary-2cce3",
-  storageBucket: "mood-diary-2cce3.firebasestorage.app",
-  messagingSenderId: "460407557020",
-  appId: "1:460407557020:web:6f082060833ae7591a9659",
-  measurementId: "G-VLYWPC9F8S"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
